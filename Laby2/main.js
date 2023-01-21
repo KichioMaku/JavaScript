b315f618b614776a2d13ce518a01dbd9b6ce2fa2
Mateusz Charewicz <charewiczmateusz@gmail.com>
2023-01-21 22:41:36 +0100

    added method to change slides

--- Laby2/main.js
+++ Laby2/main.js
@@ -1,10 +1,11 @@
 class Slider{
     constructor(images){
-        this.images = images;
+        this.imagesArray = images;
         this.slide = null;
         this.buttonNext = null;
         this.buttonPrevious = null;
         this.image = null;
+        this.imagesCounter = this.imagesArray;
 
         this.currentImage = 0;  
 
@@ -22,11 +23,39 @@ class Slider{
 
         this.image = document.createElement('img');
         this.image.classList.add('slider_image');
-        this.image.setAttribute('src', this.images[this.currentImage]);
-
-
+        this.setSlideAttributes(0);
         this.slide.appendChild(this.image);
+        this.changeSlides();
+        this.disableButtons();
+        console.log(this.imagesArray);
     }
+
+    changeSlide(index){
+        this.currentImage = index; 
+        this.setSlideAttributes(index);
+        this.disableButtons();
+    };
+
+    setSlideAttributes(index){
+        this.image.setAttribute('src', this.imagesArray[index]);
+    };
+
+    changeSlides(){
+        this.buttonPrevious.addEventListener('click', () => this.changeSlide(this.currentImage-1));
+        this.buttonNext.addEventListener('click', () => this.changeSlide(this.currentImage+1));
+    }; 
+
+    disableButtons(){
+        if(this.currentImage == 0) 
+        this.buttonPrevious.setAttribute('disabled', true) 
+        else 
+        this.buttonPrevious.removeAttribute('disabled')
+
+        if(this.currentImage == Object.keys(this.imagesArray).length-1) 
+        this.buttonNext.setAttribute('disabled', true) 
+        else 
+        this.buttonNext.removeAttribute('disabled')
+    };
 }
 
 
